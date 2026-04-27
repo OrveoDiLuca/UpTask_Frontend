ebe20a7e896f8c3b90601d61aec29160b7b4a103
Orveo <orveo1902@gmail.com>
2026-04-27 10:12:02 -0400

    Se puede eliminar los proyectos

--- src/views/DashboardView.tsx
+++ src/views/DashboardView.tsx
@@ -2,14 +2,28 @@ import { Fragment } from 'react'
 import { Menu, Transition } from '@headlessui/react'
 import { EllipsisVerticalIcon } from '@heroicons/react/20/solid'
 import { Link } from "react-router-dom"
-import { useQuery } from '@tanstack/react-query'
-import { getProjects } from "@/api/ProjectApi"
+import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query'
+import { deleteProject, getProjects } from "@/api/ProjectApi"
+import { toast } from 'react-toastify'
 
 
 export default function DashboardView() {
   const { data, isError, isLoading } = useQuery({
     queryKey: ['projects'],
     queryFn: getProjects
+  })
+
+  const queryClient = useQueryClient()
+
+  const {mutate} = useMutation({
+    mutationFn: deleteProject, 
+    onError: (error) => {
+      toast.error(error.message)
+    },
+    onSuccess: (data) => {
+      toast.success(data)
+      queryClient.invalidateQueries({queryKey: ['projects']})
+    }
   })
 
   if (data) return (
@@ -69,8 +83,8 @@ export default function DashboardView() {
                       <Menu.Item>
                         <button
                           type='button'
-                          className='block px-3 py-1 text-sm leading-6 text-red-500'
-                          onClick={() => { }}
+                          className='block px-3 py-1 text-sm leading-6 text-red-500 hover:cursor-pointer'
+                          onClick={() => mutate(project._id)}
                         >
                           Eliminar Proyecto
                         </button>
