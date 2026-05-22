a524732e957333820a53971c1a5db191978f6a13
Orveo <orveo1902@gmail.com>
2026-05-22 12:35:25 -0400

    Ahora se puede ver como historial quienes han cambiado el status del proyecto

--- src/components/tasks/TaskModalDetails.tsx
+++ src/components/tasks/TaskModalDetails.tsx
@@ -88,6 +88,17 @@ export default function TaskModalDetails() {
                                     >{data.name}
                                     </Dialog.Title>
                                     <p className='text-lg text-slate-500 mb-2'>Descripción: {data.description}</p>
+
+                                    <p className='text-lg text-slate-500 mb-2'>
+                                        Historial de Cambios
+                                    </p>
+                                    {data.completedBy.map((activityLog) => (
+                                         <p key={activityLog._id}>
+                                            <span className='font-bold text-slate-600'>{statusTranslation[activityLog.status]}</span>{' '} por:{' '}
+                                            {activityLog.user.name}
+                                        </p>
+                                    ))}
+
                                     <div className='my-5 space-y-3'>
                                         <label className='font-bold'>Estado Actual: </label>
                                         <select
