b04f3cda2266ff1aab062edd12b9d635c047416a
Orveo <orveo1902@gmail.com>
2026-05-25 10:22:00 -0400

    Se termino de realizar la parte de notas

--- src/components/notes/NoteDetail.tsx
+++ src/components/notes/NoteDetail.tsx
@@ -1,4 +1,10 @@
+import { deleteNote } from "@/api/NoteApi"
+import { useAuth } from "@/hooks/useAuth"
 import type { Note } from "@/types"
+import { useMutation, useQueryClient } from "@tanstack/react-query"
+import { useMemo } from "react"
+import { useLocation, useParams } from "react-router-dom"
+import { toast } from "react-toastify"
 
 
 type NoteDetailProps = {
@@ -7,11 +13,43 @@ type NoteDetailProps = {
 
 
 export default function NoteDetail({ note }: NoteDetailProps) {
+
+    const { data, isLoading } = useAuth()
+    const canDelete = useMemo(() => data?._id === note.createdBy._id,[data])
+    const params = useParams()
+    const location = useLocation()
+    const queryParams = new URLSearchParams(location.search)
+    const taskId = queryParams.get('viewTask')!
+    const projectId = params.projectId!
+    const queryClient = useQueryClient()
+
+    const { mutate } = useMutation({
+        mutationFn: deleteNote,
+        onError: (error) => {
+            toast.error(error.message)
+        },
+        onSuccess: (data) => {
+            toast.success(data)
+            queryClient.invalidateQueries({queryKey: ['project', taskId]})
+        }
+    })
+
+    if (isLoading) return 'Cargando...'
+
+
     return (
         <div className="p-3 flex justify-between items-center">
             <p>
                 {note.content} por: <span className="font-bold">{note.createdBy.name}</span>
             </p>
+
+            {canDelete && (
+                <button 
+                    type="button" 
+                    className="bg-red-400 cursor-pointer hover:bg-red-500 p-2 text-xs text-white font-bold transition-colors"
+                    onClick={() => mutate({projectId, taskId, noteId: note._id})}
+                >Eliminar comentario</button>
+            )}
         </div>
     )
 }
