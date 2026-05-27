f0f1eead5d7db6dd6c7a06d8a01ab44fa57afdf0
Orveo <orveo1902@gmail.com>
2026-05-27 10:36:09 -0400

    Arreglando cosas

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vite'
-import {fileURLToPath, URL} from 'node:url'
+import { fileURLToPath, URL } from 'node:url'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: '/',   // ← agrega esta línea
   resolve: {
     alias: {
       '@' : fileURLToPath(new URL('./src', import.meta.url))
